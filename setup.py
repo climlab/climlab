472297a35d295e6635a3115f31913a0c30507411
Brian Rose <brose@albany.edu>
2014-10-14 14:41:40 -0400

    Reorganized directory to use setuptools

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='climlab',
+      version='0.1',
+      description='Package for simple climate modeling',
+      url='http://github.com/brian-rose/climlab',
+      author='Brian E. J. Rose',
+      author_email='brose@albany.edu',
+      license='MIT',
+      packages=['climlab'],
+      zip_safe=False)
