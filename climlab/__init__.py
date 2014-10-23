b87b8b3c8e64981792140d266d034ada0dc9d4c6
Brian Rose <brose@albany.edu>
2014-10-23 17:33:24 -0400

    OrbitalTable class is now working.

--- climlab/__init__.py
+++ climlab/__init__.py
@@ -1,3 +1,3 @@
 #  This list defines all the modules that will be loaded if a user invokes
 #   from ClimLab import *
-__all__ = ["constants", "thermo", "orbital", "orbital_extended", "ebm", "column"]
+__all__ = ["constants", "thermo", "orbital_table", "insolation", "ebm", "column"]
