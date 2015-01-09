19315898c65c3bbbe8c9871e40343dbdbc6d02e1
Brian Rose <brose@albany.edu>
2015-01-09 15:00:28 -0500

    Moved Transmissivity class to its own module.

--- climlab/column.py
+++ climlab/column.py
@@ -11,6 +11,7 @@ import numpy as np
 import constants as const
 from convadj import convective_adjustment
 from model import _TimeSteppingModel
+from transmissivity import Transmissivity
 
 # Making this more consistent with ebm.py --
 #   no more dictionary for parameters
@@ -205,28 +206,3 @@ class Column(_TimeSteppingModel):
     #            self.Ts = Tadj[0]
     #            self.Tatm = Tadj[1:self.params['num_levels']+1]
     #        self.update_time()
-
-
-class Transmissivity:
-    '''Need to write something here.'''
-    def __init__(self, absorb):
-        if absorb.ndim is not 1:
-            raise ValueError('absorb argument must be a vector')
-        self.absorb = absorb
-        self.trans = 1 - self.absorb
-        N = self.absorb.size
-        # a matrix containing the transmission between atmospheric layers
-        #  multiply this matrix by vector of emissions to get the
-        # total incident beam at each layer.
-        self.atm2atm = np.diag(np.ones(N-1), 1)
-        for n in range(N):
-            self.atm2atm[n, n+2:N] = np.cumprod(self.trans[n+1:N-1])
-        self.atm2atm += self.atm2atm.transpose()
-
-        # the transmissivity between surface and layer k
-        self.surf2atm = np.concatenate(([1.], np.cumprod(self.trans[:N-1])))
-        # the transmissivity between layer k and space
-        self.atm2space = np.flipud(np.cumprod(np.concatenate(([1.],
-                                              np.flipud(self.trans[1:N])))))
-        #  the transmissivity between surface and space
-        self.surf2space = np.prod(self.trans)
