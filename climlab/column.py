75e6e12602ec4ca0d22f3187c7b5c3fc350ee1ac
Brian Rose <brose@albany.edu>
2015-01-12 11:44:47 -0500

    Parameters as dictionary. Timestepping stuff as dictionary. State variables as nc.Dataset.. not working.

--- climlab/column.py
+++ climlab/column.py
@@ -18,10 +18,10 @@ import flux
 class Column(_TimeSteppingModel):
     """The Column object represents surface temperature and air temperature
     on grid of pressure levels."""
-    def __str__(self):
-        return ("Instance of Column class with surface temperature " +
-                str(self.Ts) + " K, and " + str(self.num_levels) +
-                " atmospheric levels.")
+#    def __str__(self):
+        #return ("Instance of Column class with surface temperature " +
+        #        str(self.Ts) + " K, and " + str(self.num_levels) +
+        #        " atmospheric levels.")
             #  WILL WANT TO MAKE SOME BETTER OUTPUT HERE.
 #        return ("Instance of Column class with surface temperature " +
 #                str(self.Ts) + " K, " + str(self.num_levels) +
@@ -38,23 +38,27 @@ class Column(_TimeSteppingModel):
                  # absorption coefficient in m**2 / kg
                  abs_coeff=1.229E-4,
                  # lapse rate for convective adjustment, in K / km
-                 adj_lapse_rate=None):
+                 adj_lapse_rate=None,
+                 **kwargs):
+        #  First create the dataset
+        super(Column, self).__init__(**kwargs)
         #  Attach all parameters to the object
         self.p = p
-        self.num_levels = num_levels
-        self.water_depth = water_depth
-        self.albedo = albedo
-        self.Q = Q
-        self.timestep = timestep
-        self.abs_coeff = abs_coeff
-        self.adj_lapse_rate = adj_lapse_rate
+        
+        self.param['num_levels'] = num_levels
+        self.param['water_depth'] = water_depth
+        self.param['albedo'] = albedo
+        self.param['Q'] = Q
+        #self.timestep = timestep
+        self.param['abs_coeff'] = abs_coeff
+        self.param['adj_lapse_rate'] = adj_lapse_rate
 
         if p is None:
             #  pressure interval in hPa or mb
-            self.dp = const.ps / self.num_levels
+            self.dp = const.ps / self.param['num_levels']
             #  pressure at each level, in hPa or mb
             self.p = np.linspace(const.ps - self.dp/2,
-                                 self.dp/2, self.num_levels)
+                                 self.dp/2, self.param['num_levels'])
         else:
             # if pressure levels are provided:
             #  assume layer boundaries occur halfway between the given levels
@@ -63,34 +67,32 @@ class Column(_TimeSteppingModel):
             self.pbounds = np.concatenate(([const.ps], (self.p[0:N-1] +
                                           self.p[1:N])/2, [0.]))
             self.dp = np.flipud(np.diff(np.flipud(self.pbounds)))
-            self.num_levels = N
+            self.param['num_levels'] = N
 
         # initial surface temperature
         self.Ts = 288.
         # intitial column temperature
-        self.Tatm = np.linspace(self.Ts-10., 200., self.num_levels)
+        self.Tatm = np.linspace(self.Ts-10., 200., self.param['num_levels'])
         #  heat capacity of atmospheric layers
         self.c_atm = const.cp * self.dp * const.mb_to_Pa / const.g
         #  heat capacity of surface in J / m**2 / K
-        self.c_sfc = const.cw * const.rho_w * self.water_depth
+        self.c_sfc = const.cw * const.rho_w * self.param['water_depth']
         self.set_LW_emissivity()
         self.set_SW_absorptivity()
 
-        # self.steps = 0
-        # self.days_elapsed = 0.
         self.set_timestep(num_steps_per_year=const.seconds_per_year /
-                          self.timestep)
+                          timestep)
         #  A dictionary of the model state variables
-        self.state = {'Ts': self.Ts, 'Tatm': self.Tatm}
+        #self.state = {'Ts': self.Ts, 'Tatm': self.Tatm}
 
     def set_LW_emissivity(self, eps=None):
         """Set the longwave emissivity / absorptivity eps for the Column."""
         if eps is None:
             # default is to set eps equal at every level
             # use value consistent with the absorption coefficient parameter
-            self.eps = (2. / (1 + 2. * const.g / self.abs_coeff /
+            self.eps = (2. / (1 + 2. * const.g / self.param['abs_coeff'] /
                         (self.dp * const.mb_to_Pa)))
-        elif (np.isscalar(eps) or eps.size == self.num_levels):
+        elif (np.isscalar(eps) or eps.size == self.param['num_levels']):
             self.eps = eps
         else:
             raise ValueError('eps must be scalar or have exactly ' +
@@ -135,9 +137,9 @@ class Column(_TimeSteppingModel):
 
     def shortwave_heating(self):
         '''Net shortwave heating at each level.'''
-        self.SWdown_TOA = self.Q
+        self.SWdown_TOA = self.param['Q']
         absorbed_sfc, absorbed_atm, absorbed_total, incident_sfc, up2space, planetary_albedo = \
-            flux.SWflux(self.Q, self.albedo, self.SWtrans)
+            flux.SWflux(self.SWdown_TOA, self.param['albedo'], self.SWtrans)
         self.SWdown_sfc = incident_sfc
         self.SW_absorbed_sfc = absorbed_sfc
         self.SW_absorbed_atm = absorbed_atm
@@ -156,9 +158,9 @@ class Column(_TimeSteppingModel):
         self.rad_heating_sfc = self.SW_absorbed_sfc + self.LW_absorbed_sfc
         self.rad_heating_atm = self.SW_absorbed_atm + self.LW_absorbed_atm
         #  temperature tendencies due only to radiation
-        self.rad_temp_tendency_sfc = (self.rad_heating_sfc * self.timestep /
+        self.rad_temp_tendency_sfc = (self.rad_heating_sfc * self.time['timestep'] /
                                       self.c_sfc)
-        self.rad_temp_tendency_atm = (self.rad_heating_atm * self.timestep /
+        self.rad_temp_tendency_atm = (self.rad_heating_atm * self.time['timestep'] /
                                       self.c_atm)
 
     def step_forward(self):
@@ -170,16 +172,15 @@ class Column(_TimeSteppingModel):
         self.rad_temperature_tendency()
         self.Ts += self.rad_temp_tendency_sfc
         self.Tatm += self.rad_temp_tendency_atm
-        if self.adj_lapse_rate is not None:
+        if self.param['adj_lapse_rate'] is not None:
             self.unstable_Ts = self.Ts
             self.unstable_Tatm = self.Tatm
-            # Tadj = self.convective_adjustment( lapserate = self.params['adj_lapse_rate'] )
             Tcol = np.concatenate(([self.Ts], self.Tatm))
             pnew = np.concatenate(([const.ps], self.p))
             cnew = np.concatenate(([self.c_sfc], self.c_atm *
                                   np.ones_like(self.p)))
             Tadj = convective_adjustment(pnew, Tcol, cnew,
-                                         lapserate=self.adj_lapse_rate)
+                                         lapserate=self.param['adj_lapse_rate'])
             self.Ts = Tadj[0]
             self.Tatm = Tadj[1:self.num_levels+1]
         super(Column, self).step_forward()
--- climlab/model.py
+++ climlab/model.py
@@ -1,26 +1,47 @@
 import numpy as np
 import constants as const
+import netCDF4 as nc
+import time
 
 
-class _TimeSteppingModel(object):
+class _Model(object):
+    '''A generic parent class for all climlab model objects.
+    Every model object has a set of state variables on a spatial grid,
+    stored as a netCDF4 dataset.'''
+    def __init__(self, filename='ignored.nc', diskless=True, **kwargs):
+        # Create a netCDF4.Dataset object
+        self.state = nc.Dataset(filename=filename, mode="w",
+                                diskless=diskless, format='NETCDF4')
+        # Set some global attributes
+        self.state.description1 = "Model object created by climlab"
+        self.state.description2 = "Model type is " + str(type(self))
+        self.state.creation_date = time.strftime("%a, %d %b %Y %H:%M:%S %z",
+                                                 time.localtime())
+        # This dictionary will hold all model parameters
+        self.param = {}
+
+
+class _TimeSteppingModel(_Model):
     '''A generic parent class for all time-dependent models that use a
     discete forward timestep.'''
-    def __init__(self):
-        self.grid = {}
-        self.state = {}
+    def __init__(self, **kwargs):
+        # Create the state dataset
+        super(_TimeSteppingModel, self).__init__(**kwargs)
         self.set_timestep()
         #  Daughter classes will need to do a bunch of other initialization
 
     def set_timestep(self, num_steps_per_year=90):
         '''Change the timestep, given a number of steps per calendar year.'''
-        self.num_steps_per_year = num_steps_per_year
-        self.timestep = const.seconds_per_year / self.num_steps_per_year
-        timestep_days = self.timestep / const.seconds_per_day
-        self.days_of_year = np.arange(0., const.days_per_year, timestep_days)
-        self.day_of_year_index = 0
-        self.steps = 0
-        self.days_elapsed = 0.
-        self.years_elapsed = 0
+        timestep = const.seconds_per_year / num_steps_per_year
+        timestep_days = timestep / const.seconds_per_day
+        days_of_year = np.arange(0., const.days_per_year, timestep_days)
+        self.time = {'timestep': timestep,
+                     'num_steps_per_year': num_steps_per_year,
+                     'day_of_year_index': 0,
+                     'steps': 0,
+                     'days_elapsed': 0,
+                     'years_elapsed': 0,
+                     'days_of_year': days_of_year}
 
     def step_forward(self):
         '''Daughter classes need to implement details
@@ -31,44 +52,44 @@ class _TimeSteppingModel(object):
     def _update_time(self):
         '''Increment the timestep counter by one.
         This function is called by the timestepping routines.'''
-        self.steps += 1
+        self.time['steps'] += 1
         # time in days since beginning
-        self.days_elapsed += self.timestep / const.seconds_per_day
-        if self.day_of_year_index >= self.num_steps_per_year-1:
+        self.time['days_elapsed'] += self.time['timestep'] / const.seconds_per_day
+        if self.time['day_of_year_index'] >= self.time['num_steps_per_year']-1:
             self._do_new_calendar_year()
         else:
-            self.day_of_year_index += 1
+            self.time['day_of_year_index'] += 1
 
     def _do_new_calendar_year(self):
         '''This function is called once at the end of every calendar year.'''
-        self.day_of_year_index = 0  # back to Jan. 1
-        self.years_elapsed += 1
+        self.time['day_of_year_index'] = 0  # back to Jan. 1
+        self.time['years_elapsed'] += 1
 
     def integrate_years(self, years=1.0, verbose=True):
         '''Timestep the model forward a specified number of years.'''
-        numsteps = int(self.num_steps_per_year * years)
+        numsteps = int(self.time['num_steps_per_year'] * years)
         if verbose:
             print("Integrating for " + str(numsteps) + " steps or "
                   + str(years) + " years.")
         #  This implements a generic time-averaging feature
         # using the list of model state variables
-        self.state_timeave = {}
-        for varname, value in self.state.items():
-            self.state_timeave[varname] = np.zeros_like(value)
+        #self.state_timeave = {}
+        #for varname, value in self.state.items():
+        #    self.state_timeave[varname] = np.zeros_like(value)
         #  begin time loop
         for count in range(numsteps):
             self.step_forward()
-            for varname, value in self.state.items():
-                self.state_timeave[varname] += value
-        for varname, value in self.state.items():
-            self.state_timeave[varname] /= numsteps
+        #    for varname, value in self.state.items():
+        #        self.state_timeave[varname] += value
+        #for varname, value in self.state.items():
+        #    self.state_timeave[varname] /= numsteps
         if verbose:
             print("Total elapsed time is " +
-                  str(self.days_elapsed/const.days_per_year) + " years.")
+                  str(self.time['days_elapsed']/const.days_per_year) + " years.")
 
     def integrate_days(self, days=1.0, verbose=True):
         '''Timestep the model forward a specified number of days.'''
-        numsteps = int(self.num_steps_per_year / const.days_per_year * days)
+        numsteps = int(self.time['num_steps_per_year'] / const.days_per_year * days)
         if verbose:
             print("Integrating for " + str(numsteps) + " steps or " +
                   str(days) + " days.")
@@ -77,4 +98,4 @@ class _TimeSteppingModel(object):
             self.step_forward()
         if verbose:
             print("Total elapsed time is " +
-                  str(self.days_elapsed/const.days_per_year) + " years.")
+                  str(self.time['days_elapsed']/const.days_per_year) + " years.")
