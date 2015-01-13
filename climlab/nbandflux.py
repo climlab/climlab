4ff0ecb337d442a710624635567a10b72a780a51
Brian Rose <brose@albany.edu>
2015-01-13 14:20:07 -0500

    Created a flux class for N-band models. Not really using it yet.

---
+++ climlab/nbandflux.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+
+# module to compute radiative transfer in a column
+# output: flux convergence (absorbed energy) at surface and each atm box
+# output: flux out at TOA
+# maybe also some diagnostic quantities
+
+class _NbandFlux(object):
+    def __init__(self, absorb):
+        self.trans = set_transmissitivity(absorb)
+    def compute_flux(self,fromspace, albedo_sfc, emit_sfc, emit_atm):
+        absorbed, flux = _NbandFluxCompute(fromspace, albedo_sfc, emit_sfc, emit_atm, self.trans)
+        self.absorbed = absorbed
+        self.flux = flux
+
+
+def _NbandFluxCompute(fromspace, albedo_sfc, emit_sfc, emit_atm, trans):
+    flux = {}  # fluxes in W / m**2
+    absorbed = {}  # absorbed radiation (flux convergence) in W / m**2
+    flux['space2sfc'] = fromspace * trans['sfc2space']
+    flux['space2atm'] = fromspace * trans['atm2space']
+    flux['atm2sfc'] = np.dot(trans['sfc2atm'], emit_atm)
+    flux['atm2atm'] = np.dot(trans['atm2atm'], emit_atm)
+    flux['incident_sfc'] = flux['space2sfc'] + np.sum(flux['atm2sfc'])
+    flux['up_sfc'] = albedo_sfc * flux['incident_sfc'] + emit_sfc
+    flux['sfc2atm'] = flux['up_sfc'] * trans['sfc2atm']
+    flux['sfc2space'] = flux['up_sfc'] * trans['sfc2space']
+    flux['atm2space'] = emit_atm * trans['atm2space']
+    absorbed['sfc'] = flux['incident_sfc'] - flux['up_sfc']
+    absorbed['atm'] = (flux['atm2atm'] + flux['sfc2atm'] + flux['space2atm']) * trans['absorb'] - 2*emit_atm
+    absorbed['total'] = absorbed['sfc'] + np.sum(absorbed['atm'])
+    flux['up2space'] = flux['sfc2space'] + np.sum(flux['atm2space'])
+    flux['net2sfc'] = flux['incident_sfc'] - flux['up_sfc']
+    return absorbed, flux
+
+
+def set_transmissitivity(absorb):
+    '''Calculate transmissivity matrices between each level.
+    Input: 1-dimensional numpy array of absorbptivities for each level.
+
+    Returns a dictionary of numpy arrays:
+        absorb: level absorptivity (N)
+        trans: level transmissivity (N)
+        sfc2atm: transmissivity between surface and each level (N)
+        sfc2space: transmissivity between surface and space (TOA) (1)
+        atm2space: transmissivity between each level and space (N)
+        atm2atm: transmissivity matrix between atmospheric levels (N,N)
+    '''
+    if absorb.ndim is not 1:
+        raise ValueError('absorb argument must be a vector')
+    trans = 1 - absorb
+    N = absorb.size
+    # a matrix containing the transmission between atmospheric layers
+    #  multiply this matrix by vector of emissions to get the
+    # total incident beam at each layer.
+    atm2atm = np.diag(np.ones(N-1), 1)
+    for n in range(N):
+        atm2atm[n, n+2:N] = np.cumprod(trans[n+1:N-1])
+    atm2atm += atm2atm.transpose()
+    # the transmissivity between surface and layer k
+    sfc2atm = np.concatenate(([1.], np.cumprod(trans[:N-1])))
+    # the transmissivity between layer k and space
+    atm2space = np.flipud(np.cumprod(np.concatenate(([1.],
+                          np.flipud(trans[1:N])))))
+    #  the transmissivity between surface and space
+    sfc2space = np.prod(trans)
+    # Assemble all fields in a dictionary
+    transdict = {'absorb': absorb, 'trans': trans, 'atm2atm': atm2atm,
+                 'sfc2atm': sfc2atm, 'atm2space': atm2space,
+                 'sfc2space': sfc2space}
+    return transdict
--- climlab/radiation.py
+++ climlab/radiation.py
@@ -2,8 +2,8 @@ import numpy as np
 import constants as const
 from timestepping_model import _TimeSteppingModel
 import heat_capacity
-from transmissivity import set_transmissitivity
-from flux import _NbandFlux
+#from transmissivity import set_transmissitivity
+import nbandflux
 
 
 #==============================================================================
@@ -58,7 +58,7 @@ class NbandModel(_Radiation):
         '''Set or change the band absorptivity. 
         Input: eps
         must be same size as grid.'''
-        self.trans = set_transmissitivity(eps)
+        self.trans = nbandflux.set_transmissitivity(eps)
         
     def set_emissivity(self, emissivity_sfc, emissivity_atm):
         '''emissivity should be a fraction 0-1
@@ -82,7 +82,7 @@ class NbandModel(_Radiation):
         except:
             fromspace = np.zeros_like(self.state['Ts'])
         albedo_sfc = self.albedo_sfc
-        absorbed, flux = _NbandFlux(fromspace, albedo_sfc, self.diagnostics['emit_sfc'], self.diagnostics['emit_atm'], self.trans)
+        absorbed, flux = nbandflux._NbandFluxCompute(fromspace, albedo_sfc, self.diagnostics['emit_sfc'], self.diagnostics['emit_atm'], self.trans)
         self.absorbed = absorbed
         self.flux = flux
         self.diagnostics['absorbed_sfc'] = absorbed['sfc']
@@ -144,110 +144,3 @@ def compute_layer_absorptivity(abs_coeff, grid):
     return (2. / (1 + 2. * const.g / abs_coeff /
                          (grid['lev'].delta * const.mb_to_Pa)))
 
-    
-#==============================================================================
-# 
-# class GreyRadiation(_TimeSteppingModel):
-#     def __init__(self, grid=None, state=None, param=None, eps=None, abs_coeff=None, **kwargs):
-#         super(GreyRadiation, self).__init__(grid=grid, state=state, param=param, **kwargs)
-#         self.set_LW_emissivity(eps=eps, abs_coeff=self.param['abs_coeff'])
-#         self.set_SW_absorptivity(eps)
-#         #  heat capacity of atmospheric layers
-#         self.c_atm = heat_capacity.atmosphere(self.grid['lev'].delta)
-#         #  heat capacity of surface in J / m**2 / K
-#         self.c_sfc = heat_capacity.slab_ocean(self.param['water_depth'])
-# 
-#     def set_LW_emissivity(self, eps=None, abs_coeff=None):
-#         """Set the longwave emissivity / absorptivity eps for the Column."""
-#         if eps is None:
-#             # default is to set eps equal at every level
-#             # use value consistent with the absorption coefficient parameter
-#             self.eps = (2. / (1 + 2. * const.g / abs_coeff /
-#                         (self.grid['lev'].delta * const.mb_to_Pa)))
-#         elif (np.isscalar(eps) or eps.size == self.param['num_levels']):
-#             self.eps = eps
-#         else:
-#             raise ValueError('eps must be scalar or have exactly ' +
-#                              self.num_levels + ' elements.')
-# 
-#         if np.isscalar(self.eps):
-#             self.LWtrans = set_transmissitivity(self.eps * np.ones_like(self.grid['lev'].points))
-#         else:
-#             self.LWtrans = set_transmissitivity(self.eps)
-# 
-#     def set_SW_absorptivity(self, eps=None):
-#         """Set the shortwave absorptivity eps for the Column."""
-#         if eps is None:
-#             # default is no shortwave absorption
-#             self.SWtrans = set_transmissitivity(np.zeros_like(self.grid['lev'].points))
-#         elif np.isscalar(eps):
-#             # passing a single scalar sets eps equal to this number everywhere
-#             self.SWtrans = set_transmissitivity(self.eps * np.ones_like(self.grid['lev'].points))
-#         elif np.size(eps) == self.param['num_levels']:
-#             self.SWtrans = set_transmissitivity(eps)
-#         else:
-#             raise ValueError('eps must be scalar or have exactly ' +
-#                              self.param['num_levels'] + ' elements.')
-#     
-#     def emission(self):
-#         emit_sfc = const.sigma * self.state['Ts']**4
-#         emit_atm = const.sigma * self.state['Tatm']**4
-#         self.diagnostics['emit_sfc'] = emit_sfc
-#         self.diagnostics['emit_atm'] = emit_atm
-#     
-#     def longwave_heating(self):
-#         """Compute the net longwave radiative heating at every level
-#         and the surface. Also store the upwelling longwave radiation at the top
-#         (OLR), and the downwelling longwave radiation at the surface.
-#         """
-#         eps = self.LWtrans['absorb']
-#         # emissions from surface and each layer
-#         self.emission()
-#         absorbed_sfc, absorbed_atm, OLR, LWdown_sfc, OLR_sfc, OLR_atm = \
-#             flux.LWflux(self.diagnostics['emit_atm'], 
-#                         self.diagnostics['emit_sfc'], 
-#                         self.LWtrans)
-#         self.diagnostics['LW_down_sfc'] = LWdown_sfc
-#         self.diagnostics['OLR_sfc'] = OLR_sfc
-#         self.diagnostics['OLR_atm'] = OLR_atm
-#         self.diagnostics['OLR'] = OLR
-#         self.diagnostics['LW_absorbed_sfc'] = absorbed_sfc
-#         self.diagnostics['LW_absorbed_atm'] = absorbed_atm
-#     
-#     def shortwave_heating(self):
-#         '''Net shortwave heating at each level.'''
-#         self.diagnostics['SWdown_TOA'] = self.param['Q']
-#         absorbed_sfc, absorbed_atm, absorbed_total, incident_sfc, up2space, planetary_albedo = \
-#             flux.SWflux(self.diagnostics['SWdown_TOA'],
-#                         self.param['albedo'],
-#                         self.SWtrans)
-#         self.diagnostics['SWdown_sfc'] = incident_sfc
-#         self.diagnostics['SW_absorbed_sfc'] = absorbed_sfc
-#         self.diagnostics['SW_absorbed_atm'] = absorbed_atm
-#         self.diagnostics['SWup_TOA'] = up2space
-#         self.diagnostics['SW_absorbed_total'] = absorbed_total
-#         self.diagnostics['planetary_albedo'] = planetary_albedo
-#     
-#     def rad_temperature_tendency(self):
-#         """Compute net radiative heating everywhere in the Column (in W/m^2),
-#         and the resulting temperature change over a specified timestep (in K).
-#         """
-#         # compute longwave heating rates
-#         self.longwave_heating()
-#         self.shortwave_heating()
-#         # net radiative forcing
-#         self.diagnostics['rad_heating_sfc'] = (self.diagnostics['SW_absorbed_sfc'] 
-#                                         + self.diagnostics['LW_absorbed_sfc'])
-#         self.diagnostics['rad_heating_atm'] = (self.diagnostics['SW_absorbed_atm'] 
-#                                         + self.diagnostics['LW_absorbed_atm'])
-#         #  temperature tendencies due only to radiation
-#         self.diagnostics['rad_temp_tendency_sfc'] = (self.diagnostics['rad_heating_sfc']
-#                                     * self.param['timestep'] / self.c_sfc)
-#         self.diagnostics['rad_temp_tendency_atm'] = (self.diagnostics['rad_heating_atm']
-#                                     * self.param['timestep'] / self.c_atm)
-#     
-#     def compute(self):
-#       self.rad_temperature_tendency()
-#       self.tendencies['Ts'] = self.diagnostics['rad_temp_tendency_sfc']
-#       self.tendencies['Tatm'] = self.diagnostics['rad_temp_tendency_atm']
-#==============================================================================
