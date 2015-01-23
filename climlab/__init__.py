76b892055e24e30824e84c3d6b74aaa7303d28bb
Brian Rose <brose@albany.edu>
2015-01-23 09:16:00 -0500

    Added shortcuts to __init__.py. Got rid of old grid.py

--- climlab/__init__.py
+++ climlab/__init__.py
@@ -1,14 +1,23 @@
+__version__ = '0.2'
+
 #  This list defines all the modules that will be loaded if a user invokes
 #   from climLab import *
 
 # totally out of date!
 
-__all__ = ["constants", "thermo", "orbital_table",
-           "long_orbital_table", "insolation", "ebm",
-           "column", "convadj"]
+#__all__ = ["constants", "thermo", "orbital_table",
+#           "long_orbital_table", "insolation", "ebm",
+#           "column", "convadj"]
 
 # this should ensure that we can still import constants.py as climlab.constants           
 from climlab.utils import constants, thermo
-
-__version__ = '0.2'
-
+# some more useful shorcuts
+from climlab.model import ebm, column
+from climlab.domain import domain
+from climlab.domain.field import Field, global_mean
+from climlab.domain.axis import Axis
+from climlab.process.process import Process, process_like, get_axes
+from climlab.process.time_dependent_process import TimeDependentProcess
+from climlab.process.implicit import ImplicitProcess
+from climlab.process.diagnostic import DiagnosticProcess
+from climlab.process.energy_budget import EnergyBudget
