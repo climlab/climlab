5fc66dbfcbb1978f3281c4790bc1428b2a9fd0ce
Brian Rose <brose@albany.edu>
2015-02-04 22:21:14 -0500

    Another small reorganization of nband flux computation code.

--- climlab/radiation/nband.py
+++ climlab/radiation/nband.py
@@ -59,12 +59,34 @@ class NbandModel(_Radiation):
         except:
             fromspace = np.zeros_like(self.state['Ts'])
         albedo_sfc = self.albedo_sfc
-        absorbed, flux = self._trans.FluxCompute(fromspace, albedo_sfc, 
+        absorbed, flux = flux_compute(fromspace, albedo_sfc, 
                                             self.diagnostics['emit_sfc'], 
-                                            self.diagnostics['emit_atm'])
+                                            self.diagnostics['emit_atm'],
+                                            self._trans)
         self.absorbed = absorbed
         self.flux = flux
         self.diagnostics['absorbed_sfc'] = absorbed['sfc']
         self.diagnostics['absorbed_atm'] = absorbed['atm']
         self.heating_rate['Ts'] = absorbed['sfc']
         self.heating_rate['Tatm'] = absorbed['atm']
+
+
+def flux_compute(fromspace, albedo_sfc, emit_sfc, emit_atm, trans):
+    flux = {}  # fluxes in W / m**2
+    absorbed = {}  # absorbed radiation (flux convergence) in W / m**2
+    flux['space2sfc'] = fromspace * trans.sfc2space
+    flux['space2atm'] = fromspace * trans.atm2space
+    flux['atm2sfc'] = np.dot(trans.sfc2atm, emit_atm)
+    flux['atm2atm'] = np.dot(trans.atm2atm, emit_atm)
+    flux['incident_sfc'] = flux['space2sfc'] + np.sum(flux['atm2sfc'])
+    flux['up_sfc'] = albedo_sfc * flux['incident_sfc'] + emit_sfc
+    flux['sfc2atm'] = flux['up_sfc'] * trans.sfc2atm
+    flux['sfc2space'] = flux['up_sfc'] * trans.sfc2space
+    flux['atm2space'] = emit_atm * trans.atm2space
+    absorbed['sfc'] = flux['incident_sfc'] - flux['up_sfc']
+    absorbed['atm'] = ((flux['atm2atm'] + flux['sfc2atm'] + flux['space2atm'])
+                       * trans.absorptivity - 2*emit_atm)
+    absorbed['total'] = absorbed['sfc'] + np.sum(absorbed['atm'])
+    flux['up2space'] = flux['sfc2space'] + np.sum(flux['atm2space'])
+    flux['net2sfc'] = flux['incident_sfc'] - flux['up_sfc']
+    return absorbed, flux
