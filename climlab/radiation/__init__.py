72fd4ce75772f5b4dc2efb236b9bd0eb764bcce8
Brian Rose <brose@albany.edu>
2015-02-24 13:15:44 -0500

    Fixed bug with import climlab due to missing grey_radiation.py module

--- climlab/radiation/__init__.py
+++ climlab/radiation/__init__.py
@@ -1,4 +1,3 @@
 from AplusBT import AplusBT
-from grey_radiation import GreyRadiation_LW, GreyRadiation_SW
 from insolation import FixedInsolation, P2Insolation, AnnualMeanInsolation, DailyInsolation
 from radiation import Radiation
