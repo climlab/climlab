3802af67a2dd36ca4987fa343167a6991965b21e
Brian Rose <brose@albany.edu>
2015-03-01 17:31:30 -0500

    Complete NbandRadiation class ... ready to implement few-band longwave and shortwave models.

--- climlab/radiation/nband.py
+++ climlab/radiation/nband.py
@@ -12,7 +12,7 @@ class NbandRadiation(Radiation):
     with the spectrum divided into N spectral bands.
     
     Every NbandRadiation object has an attribute
-    self.band_fraction
+        self.band_fraction
     with sum(self.band_fraction) == 1
     that gives the fraction of the total beam in each band
     
@@ -25,19 +25,12 @@ class NbandRadiation(Radiation):
         self.absorption_cross_section
     that gives the absorption cross-section per unit mass for each gas
     in every spectral band
-    
-    
     '''
     def __init__(self, **kwargs):
         super(NbandRadiation, self).__init__(**kwargs)
         #self.numSWchannels = 3
-        self.num_channels = self.band_fraction.size        
-        # abstract axis for channels
-        #ax = axis.Axis(num_points=self.num_channels)
-        #self.channel_ax = {'channel': ax}
-        #dom = domain._Domain(axes=self.channel_ax)
-        #   fraction of the total solar flux in each band:
-        #self.band_fraction = field.Field([0.01, 0.27, 0.72], domain=dom)
+        # this should be overridden by daughter classes
+        self.band_fraction = np.array(1.)
         ##  a dictionary of absorbing gases, in volumetric mixing ratios
         #  each item should have dimensions of self.Tatm
         self.absorber_vmr = {}
@@ -51,58 +44,54 @@ class NbandRadiation(Radiation):
         #                           (self.numSWchannels, 1))
         #self.sigmaO3 = np.reshape(np.array([200.E-24, 0.285E-24, 0.]) * 
         #    const.Rd / const.kBoltzmann, (self.numSWchannels, 1))
-        self.cosZen = 0.5  # cosine of the average solar zenith angle
-        self.p = self.lev
+        self.cosZen = 1.  # cosine of the average zenith angle
         dp = self.Tatm.domain.lev.delta
         self.mass_per_layer = dp * const.mb_to_Pa / const.g
-        #self.relative_humidity = 0.77  # fixed relative humidity at surface
-        #self.qStrat = 5.E-6  # minimum specific humidity for stratosphere
         self.flux_from_space = np.zeros_like(self.Ts)
         self.flux_from_sfc = np.zeros_like(self.Ts)
         self.albedo_sfc = np.ones_like(self.band_fraction)*self.albedo_sfc
         self.compute_absorptivity()
-
-    def Manabe_water_vapor(self):
-        '''Compute water vapor mixing ratio profile following 
-        Manabe and Wetherald JAS 1967
-        Fixed surface relative humidity and a specified fractional profile.'''
-        Q = self.p / const.ps
-        h = self.relative_humidity * ( (Q - 0.02) / (1-0.02) )
-        es = clausius_clapeyron( self.Tatm )
-        e = h * es
-        # convert to specific humidity (assume dilute)
-        qH2O = e / self.p * const.Rd / const.Rv 
-        #  mixing ratio can't be smaller than qStrat (need some water in the stratosphere!)
-        return np.maximum( self.qStrat, qH2O )
     
-    def compute_optical_path(self,O3vmr, H2Ovmr, cosZen ):
-        '''O3vmr and H2Ovmr are mixing ratios (dimensionless arrays)
-        cosZen is the solar zenith angle.'''
-        qO3 = O3vmr / (1+O3vmr)
-        qH2O = H2Ovmr / (1+H2Ovmr)
-        return ((qO3*self.sigmaO3 + qH2O*self.sigmaH2O) / cosZen * 
-                self.mass_per_layer)
+    @property 
+    def band_fraction(self):
+        return self._band_fraction
+    @band_fraction.setter
+    def band_fraction(self, value):
+        self.num_channels = value.size        
+        # abstract axis for channels
+        ax = axis.Axis(num_points=self.num_channels)
+        self.channel_ax = {'channel': ax}
+        dom = domain._Domain(axes=self.channel_ax)
+        #   fraction of the total solar flux in each band:
+        self._band_fraction = field.Field(self.band_fraction, domain=dom)    
+
+    def compute_optical_path(self):
+        tau = np.zeros_like(self.absorber_vmr['CO2']*
+                            self.absorption_cross_section['CO2'])
+        for gas, vmr in self.absorber_vmr.iteritems:
+            # convert to mass of absorber per unit total mass
+            q = vmr / (1.+vmr)
+            kappa = self.absorption_cross_section[gas]
+            tau += q * kappa
+        tau *= self.mass_per_layer / self.cosZen
+        return tau
 
     def compute_absorptivity(self):
-        #  need to recompute transmissivities each time because 
-        # water vapor is changing
-        self.H2Ovmr = self.Manabe_water_vapor()
-        optical_path = self.compute_optical_path(self.O3vmr, self.H2Ovmr,
-                                                self.cosZen )
-        #epsSW = 1. - np.exp(-optical_path)
+        #  assume that the water vapor etc is current
+        optical_path = self.compute_optical_path()
+        #  account for finite layer depth
+        absorptivity = 1. - np.exp(-optical_path)
         axes = copy(self.Tatm.domain.axes)
         # add these to the dictionary of axes
         axes.update(self.channel_ax)
         dom = domain.Atmosphere(axes=axes)
-        #self.absorptivity = field.Field(epsSW, domain=dom)
-        self.absorptivity = field.Field(optical_path, domain=dom)
+        self.absorptivity = field.Field(absorptivity, domain=dom)
     
     def radiative_heating(self):
         #  need to recompute transmissivities each time because 
         # water vapor is changing
         self.compute_absorptivity()
         self.emission = self.compute_emission()
-        #self.diagnostics['emission'] = emission
         try:
             fromspace = self.split_channels(self.flux_from_space)
         except:
@@ -127,3 +116,23 @@ class NbandRadiation(Radiation):
     
     def split_channels(self, flux):
         return (self.band_fraction*flux)[..., np.newaxis]
+
+
+def Manabe_water_vapor(Tatm, relative_humidity=0.77, qStrat=5.E-6):
+    '''Compute water vapor mixing ratio profile following 
+    Manabe and Wetherald JAS 1967
+    Fixed surface relative humidity and a specified fractional profile.
+    
+    relative_humidity is the specified surface RH
+    qStrat is the minimum specific humidity, ensuring that there is 
+    some water vapor in the stratosphere.'''
+    p = Tatm.domain.lev
+    Q = p / const.ps
+    h = relative_humidity * ( (Q - 0.02) / (1-0.02) )
+    es = clausius_clapeyron( Tatm )
+    e = h * es
+    # convert to specific humidity (assume dilute)
+    qH2O = e/p * const.Rd / const.Rv 
+    #  mixing ratio can't be smaller than qStrat 
+    #  (need some water in the stratosphere!)
+    return np.maximum( qStrat, qH2O )
