7658e7cb4c6c730cc826127e7ca518a2bb6f716e
cjcardinale <ccardinale@albany.edu>
2016-06-30 09:54:36 -0400

    amend test
    will cover more lines

--- climlab/tests/test_domain2D.py
+++ climlab/tests/test_domain2D.py
@@ -5,6 +5,8 @@ import pytest
 
 def test_state():
     sfc = domain.surface_2D(num_lat=90, num_lon=180)
+    sfc = domain.surface_2D(lat=([-90.,0.,90.]),
+                            lon=([-180.,0.,180.]))
     state = initial.surface_state_2D(num_lat=90, num_lon=180)
     assert state.Ts.ndim == 3
     assert state.Ts[:,0,0].shape == (90,)
