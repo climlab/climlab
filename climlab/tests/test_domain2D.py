d86e0ce6bcb7b66a3bc267713c94eee38bdd2d8c
Brian Rose <brose@albany.edu>
2016-06-30 17:20:09 -0400

    New test for global mean of 2D temperature field.

--- climlab/tests/test_domain2D.py
+++ climlab/tests/test_domain2D.py
@@ -1,11 +1,14 @@
+import numpy as np
 import climlab
 from climlab import domain
 import pytest
 
 def test_state():
+    initialT0 = 15.
     sfc = domain.surface_2D(num_lat=90, num_lon=180)
     sfc = domain.surface_2D(lat=([-90.,0.,90.]),
                             lon=([-180.,0.,180.]))
-    state = climlab.surface_state(num_lat=90, num_lon=180)
+    state = climlab.surface_state(T0 = initialT0, num_lat=90, num_lon=180)
     assert state.Ts.ndim == 3
     assert state.Ts.shape == (90, 180, 1)
+    assert np.isclose(climlab.global_mean(state.Ts), initialT0, atol=1E-02)
