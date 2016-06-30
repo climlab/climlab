2e5c21106151ac0859cda0e9fbaeae433ec8ea41
cjcardinale <ccardinale@albany.edu>
2016-06-30 09:14:34 -0400

    domain and state variable test

---
+++ climlab/tests/test_domain2D.py
@@ -0,0 +1,12 @@
+import climlab
+from climlab import domain
+from climlab.domain import initial
+import pytest
+
+def test_state():
+    sfc = domain.surface_2D(num_lat=90, num_lon=180)
+    state = initial.surface_state_2D(num_lat=90, num_lon=180)
+    assert state.Ts.ndim == 3
+    assert state.Ts[:,0,0].shape == (90,)
+    assert state.Ts[0,:,0].shape == (180,)
+    
