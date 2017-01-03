12f8bb9a04007e302a3bc5abb995b803c77115fe
Brian Rose <brose@albany.edu>
2017-01-03 09:24:02 -0500

    Little cleanup.

--- climlab/radiation/_cam3_interface.py
+++ climlab/radiation/_cam3_interface.py
@@ -160,7 +160,6 @@ def _patch_extension_rpath(extname, verbose=False):
         ####   with gcc compilers installed by conda
         ####   see https://github.com/ContinuumIO/anaconda-issues/issues/739#issuecomment-238076905
     patch_rpath = []
-    #patch_rpath.append('install_name_tool -change @rpath/./libgfortran.3.dylib ${CONDA_PREFIX}/lib/libgfortran.3.dylib -change @rpath/./libquadmath.0.dylib ${CONDA_PREFIX}/lib/libquadmath.0.dylib')
     patch_rpath.append('install_name_tool -change @rpath/./libgfortran.3.dylib ${CONDA_PREFIX}/lib/libgfortran.3.dylib')
     patch_rpath.append('-change @rpath/./libquadmath.0.dylib ${CONDA_PREFIX}/lib/libquadmath.0.dylib %s' %extname)
     patch_rpath = string.join(patch_rpath)
