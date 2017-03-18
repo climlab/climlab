a105e9013a56a5152cb43dd6b021fadb1d1aa511
Brian Rose <brose@albany.edu>
2017-03-18 16:47:07 -0400

    New python driver mirroring the old fortran driver.

---
+++ climlab/radiation/rrtm/_rrtmg_lw/newdriver.py
@@ -0,0 +1,49 @@
+from rrtmg_lw_init.rrtmg_lw_init import rrtmg_lw_ini
+from mcica_subcol_gen_lw.mcica_subcol_gen_lw import mcica_subcol_lw
+from rrtmg_lw_rad.rrtmg_lw_rad import rrtmg_lw
+from parrrtm.parrrtm import nbndlw, ngptlw
+
+def driver(ncol, nlay, icld, permuteseed, irng, idrv, cpdair,
+        play, plev, tlay, tlev, tsfc,
+        h2ovmr, o3vmr, co2vmr, ch4vmr, n2ovmr, o2vmr,
+        cfc11vmr, cfc12vmr, cfc22vmr, ccl4vmr, emis,
+        inflglw, iceflglw, liqflglw,
+        cldfrac, ciwp, clwp, reic, relq, tauc, tauaer,):
+    #    ! Call the Monte Carlo Independent Column Approximation
+    #    !   (McICA, Pincus et al., JC, 2003)
+    cldfmcl = np.zeros((ngptlw,ncol,nlay))
+    ciwpmcl = np.zeros((ngptlw,ncol,nlay))
+    clwpmcl = np.zeros((ngptlw,ncol,nlay))
+    reicmcl = np.zeros((ncol,nlay))
+    relqmcl = np.zeros((ncol,nlay))
+    taucmcl = np.zeros((ngptlw,ncol,nlay))
+    mcica_subcol_lw(1, ncol, nlay, icld, permuteseed, irng, play,
+                       cldfrac, ciwp, clwp, reic, relq, tauc, cldfmcl,
+                       ciwpmcl, clwpmcl, reicmcl, relqmcl, taucmcl)
+
+    #! In principle the init routine should not need to be called every timestep
+    #!  But calling it from Python is not working... heatfac is not getting set properly
+    rrtmg_lw_ini(cpdair)
+
+    #  Initialize return arrays
+    uflx = np.zeros((ncol,nlay+1))
+    dflx = np.zeros((ncol,nlay+1))
+    hr = np.zeros((ncol,nlay))
+    uflxc = np.zeros((ncol,nlay+1))
+    dflxc = np.zeros((ncol,nlay+1))
+    hrc = np.zeros(ncol,nlay)
+    duflx_dt = np.zeros(ncol,nlay+1)
+    duflxc_dt = np.zeros(ncol,nlay+1)
+
+    #!  Call the RRTMG_LW driver to compute radiative fluxes
+    rrtmg_lw(ncol    ,nlay    ,icld    ,idrv    ,
+             play    , plev    , tlay    , tlev    , tsfc    ,
+             h2ovmr  , o3vmr   , co2vmr  , ch4vmr  , n2ovmr  , o2vmr ,
+             cfc11vmr, cfc12vmr, cfc22vmr, ccl4vmr , emis    ,
+             inflglw , iceflglw, liqflglw, cldfmcl ,
+             taucmcl , ciwpmcl , clwpmcl , reicmcl , relqmcl ,
+             tauaer  ,
+             uflx    , dflx    , hr      , uflxc   , dflxc,  hrc,
+             duflx_dt,duflxc_dt )
+
+    return uflx, dflx, hr, uflxc, dflxc, hrc, duflx_dt, duflxc_dt
---
+++ climlab/radiation/rrtm/_rrtmg_lw/parrrtm.py
@@ -0,0 +1,529 @@
+import _parrrtm
+import f90wrap.runtime
+import logging
+
+class Parrrtm(f90wrap.runtime.FortranModule):
+    """
+    Module parrrtm
+    
+    
+    Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 lines 2-110
+    
+    """
+    @property
+    def mxlay(self):
+        """
+        Element mxlay ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 31
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__mxlay()
+    
+    @property
+    def mg(self):
+        """
+        Element mg ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 32
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__mg()
+    
+    @property
+    def nbndlw(self):
+        """
+        Element nbndlw ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 33
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__nbndlw()
+    
+    @property
+    def maxxsec(self):
+        """
+        Element maxxsec ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 34
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__maxxsec()
+    
+    @property
+    def mxmol(self):
+        """
+        Element mxmol ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 35
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__mxmol()
+    
+    @property
+    def maxinpx(self):
+        """
+        Element maxinpx ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 36
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__maxinpx()
+    
+    @property
+    def nmol(self):
+        """
+        Element nmol ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 37
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__nmol()
+    
+    @property
+    def ngptlw(self):
+        """
+        Element ngptlw ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 39
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ngptlw()
+    
+    @property
+    def ng1(self):
+        """
+        Element ng1 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 44
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ng1()
+    
+    @property
+    def ng2(self):
+        """
+        Element ng2 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 45
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ng2()
+    
+    @property
+    def ng3(self):
+        """
+        Element ng3 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 46
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ng3()
+    
+    @property
+    def ng4(self):
+        """
+        Element ng4 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 47
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ng4()
+    
+    @property
+    def ng5(self):
+        """
+        Element ng5 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 48
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ng5()
+    
+    @property
+    def ng6(self):
+        """
+        Element ng6 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 49
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ng6()
+    
+    @property
+    def ng7(self):
+        """
+        Element ng7 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 50
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ng7()
+    
+    @property
+    def ng8(self):
+        """
+        Element ng8 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 51
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ng8()
+    
+    @property
+    def ng9(self):
+        """
+        Element ng9 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 52
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ng9()
+    
+    @property
+    def ng10(self):
+        """
+        Element ng10 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 53
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ng10()
+    
+    @property
+    def ng11(self):
+        """
+        Element ng11 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 54
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ng11()
+    
+    @property
+    def ng12(self):
+        """
+        Element ng12 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 55
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ng12()
+    
+    @property
+    def ng13(self):
+        """
+        Element ng13 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 56
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ng13()
+    
+    @property
+    def ng14(self):
+        """
+        Element ng14 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 57
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ng14()
+    
+    @property
+    def ng15(self):
+        """
+        Element ng15 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 58
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ng15()
+    
+    @property
+    def ng16(self):
+        """
+        Element ng16 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 59
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ng16()
+    
+    @property
+    def ngs1(self):
+        """
+        Element ngs1 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 61
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ngs1()
+    
+    @property
+    def ngs2(self):
+        """
+        Element ngs2 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 62
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ngs2()
+    
+    @property
+    def ngs3(self):
+        """
+        Element ngs3 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 63
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ngs3()
+    
+    @property
+    def ngs4(self):
+        """
+        Element ngs4 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 64
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ngs4()
+    
+    @property
+    def ngs5(self):
+        """
+        Element ngs5 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 65
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ngs5()
+    
+    @property
+    def ngs6(self):
+        """
+        Element ngs6 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 66
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ngs6()
+    
+    @property
+    def ngs7(self):
+        """
+        Element ngs7 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 67
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ngs7()
+    
+    @property
+    def ngs8(self):
+        """
+        Element ngs8 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 68
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ngs8()
+    
+    @property
+    def ngs9(self):
+        """
+        Element ngs9 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 69
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ngs9()
+    
+    @property
+    def ngs10(self):
+        """
+        Element ngs10 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 70
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ngs10()
+    
+    @property
+    def ngs11(self):
+        """
+        Element ngs11 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 71
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ngs11()
+    
+    @property
+    def ngs12(self):
+        """
+        Element ngs12 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 72
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ngs12()
+    
+    @property
+    def ngs13(self):
+        """
+        Element ngs13 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 73
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ngs13()
+    
+    @property
+    def ngs14(self):
+        """
+        Element ngs14 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 74
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ngs14()
+    
+    @property
+    def ngs15(self):
+        """
+        Element ngs15 ftype=integer(kind=im) pytype=int
+        
+        
+        Defined at rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90 line 75
+        
+        """
+        return _parrrtm.f90wrap_parrrtm__get__ngs15()
+    
+    def __str__(self):
+        ret = ['<parrrtm>{\n']
+        ret.append('    mxlay : ')
+        ret.append(repr(self.mxlay))
+        ret.append(',\n    mg : ')
+        ret.append(repr(self.mg))
+        ret.append(',\n    nbndlw : ')
+        ret.append(repr(self.nbndlw))
+        ret.append(',\n    maxxsec : ')
+        ret.append(repr(self.maxxsec))
+        ret.append(',\n    mxmol : ')
+        ret.append(repr(self.mxmol))
+        ret.append(',\n    maxinpx : ')
+        ret.append(repr(self.maxinpx))
+        ret.append(',\n    nmol : ')
+        ret.append(repr(self.nmol))
+        ret.append(',\n    ngptlw : ')
+        ret.append(repr(self.ngptlw))
+        ret.append(',\n    ng1 : ')
+        ret.append(repr(self.ng1))
+        ret.append(',\n    ng2 : ')
+        ret.append(repr(self.ng2))
+        ret.append(',\n    ng3 : ')
+        ret.append(repr(self.ng3))
+        ret.append(',\n    ng4 : ')
+        ret.append(repr(self.ng4))
+        ret.append(',\n    ng5 : ')
+        ret.append(repr(self.ng5))
+        ret.append(',\n    ng6 : ')
+        ret.append(repr(self.ng6))
+        ret.append(',\n    ng7 : ')
+        ret.append(repr(self.ng7))
+        ret.append(',\n    ng8 : ')
+        ret.append(repr(self.ng8))
+        ret.append(',\n    ng9 : ')
+        ret.append(repr(self.ng9))
+        ret.append(',\n    ng10 : ')
+        ret.append(repr(self.ng10))
+        ret.append(',\n    ng11 : ')
+        ret.append(repr(self.ng11))
+        ret.append(',\n    ng12 : ')
+        ret.append(repr(self.ng12))
+        ret.append(',\n    ng13 : ')
+        ret.append(repr(self.ng13))
+        ret.append(',\n    ng14 : ')
+        ret.append(repr(self.ng14))
+        ret.append(',\n    ng15 : ')
+        ret.append(repr(self.ng15))
+        ret.append(',\n    ng16 : ')
+        ret.append(repr(self.ng16))
+        ret.append(',\n    ngs1 : ')
+        ret.append(repr(self.ngs1))
+        ret.append(',\n    ngs2 : ')
+        ret.append(repr(self.ngs2))
+        ret.append(',\n    ngs3 : ')
+        ret.append(repr(self.ngs3))
+        ret.append(',\n    ngs4 : ')
+        ret.append(repr(self.ngs4))
+        ret.append(',\n    ngs5 : ')
+        ret.append(repr(self.ngs5))
+        ret.append(',\n    ngs6 : ')
+        ret.append(repr(self.ngs6))
+        ret.append(',\n    ngs7 : ')
+        ret.append(repr(self.ngs7))
+        ret.append(',\n    ngs8 : ')
+        ret.append(repr(self.ngs8))
+        ret.append(',\n    ngs9 : ')
+        ret.append(repr(self.ngs9))
+        ret.append(',\n    ngs10 : ')
+        ret.append(repr(self.ngs10))
+        ret.append(',\n    ngs11 : ')
+        ret.append(repr(self.ngs11))
+        ret.append(',\n    ngs12 : ')
+        ret.append(repr(self.ngs12))
+        ret.append(',\n    ngs13 : ')
+        ret.append(repr(self.ngs13))
+        ret.append(',\n    ngs14 : ')
+        ret.append(repr(self.ngs14))
+        ret.append(',\n    ngs15 : ')
+        ret.append(repr(self.ngs15))
+        ret.append('}')
+        return ''.join(ret)
+    
+    _dt_array_initialisers = []
+    
+
+parrrtm = Parrrtm()
+
