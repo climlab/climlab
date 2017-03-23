ce13435fd840ad6985d2e146f5e63b74afca2912
Brian Rose <brose@albany.edu>
2017-03-22 23:06:07 -0400

    First successful build using numpy.distutils.core.setup!

--- climlab/radiation/rrtm/_rrtmg_lw/setup.py
+++ climlab/radiation/rrtm/_rrtmg_lw/setup.py
@@ -12,25 +12,62 @@ def configuration(parent_package='', top_path=None):
     # set some fortran compiler-dependent flags (following CliMT code here)
     if compiler == 'gnu95':
         f77flags=['-ffixed-line-length-132', '-fdefault-real-8']
-        #f90flags='-fdefault-real-8 -fno-range-check -ffree-form'
-        f90flags=['-fdefault-real-8']
+        f90flags=['-fdefault-real-8', '-fno-range-check', '-ffree-form']
+        #f90flags=['-fdefault-real-8']
     elif compiler == 'intel' or compiler == 'intelem':
         f77flags='-132 -r8'
         f90flags='-132 -r8'
     print f90flags
 
-    here = os.path.abspath(os.path.dirname(__file__))
-    sourcelistfile = os.path.join(here, 'sources_in_order_of_compilation')
-    sourcelist = [line.rstrip() for line in open(sourcelistfile)]
-    sourcelist.append('_rrtmg_lw.pyf')
+    #here = os.path.abspath(os.path.dirname(__file__))
+    #sourcelistfile = os.path.join(here, 'sources_in_order_of_compilation')
+    #sourcelist = [line.rstrip() for line in open(sourcelistfile)]
+    #sourcelist.append('_rrtmg_lw.pyf')
+    sourcelist = ['_rrtmg_lw.pyf',
+                  'rrtmg_lw_v4.85/gcm_model/modules/parkind.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/parrrtm.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_cld.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_con.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_kg01.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_kg02.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_kg03.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_kg04.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_kg05.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_kg06.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_kg07.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_kg08.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_kg09.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_kg10.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_kg11.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_kg12.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_kg13.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_kg14.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_kg15.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_kg16.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_ref.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_tbl.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_vsn.f90',
+                  'rrtmg_lw_v4.85/gcm_model/modules/rrlw_wvn.f90',
+                  'rrtmg_lw_v4.85/gcm_model/src/rrtmg_lw_taumol.f90',
+                  'rrtmg_lw_v4.85/gcm_model/src/rrtmg_lw_setcoef.f90',
+                  'rrtmg_lw_v4.85/gcm_model/src/rrtmg_lw_rtrnmc.f90',
+                  'rrtmg_lw_v4.85/gcm_model/src/rrtmg_lw_cldprmc.f90',
+                  'rrtmg_lw_v4.85/gcm_model/src/mcica_random_numbers.f90',
+                  'rrtmg_lw_v4.85/gcm_model/src/mcica_subcol_gen_lw.f90',
+                  'rrtmg_lw_v4.85/gcm_model/src/rrtmg_lw_init.f90',
+                  'rrtmg_lw_v4.85/gcm_model/src/rrtmg_lw_rad.f90',
+                  'f90wrap_parrrtm.f90',
+                  'f90wrap_mcica_subcol_gen_lw.f90',
+                  'f90wrap_rrtmg_lw_init.f90',
+                  'f90wrap_rrtmg_lw_rad.f90']
 
     config = Configuration(package_name='_rrtmg_lw', parent_name=parent_package, top_path=top_path)
     config.add_extension(name='_rrtmg_lw',
                          sources=sourcelist,
-                         extra_f90_compile_args=['-fdefault-real-8'],)
+                         extra_f90_compile_args=f90flags,)
                          #f2py_options=['-c',])
-    config.add_include_dirs([os.path.join(here,'rrtmg_lw_v4.85','gcm_model','src'),
-                             os.path.join(here,'rrtmg_lw_v4.85','gcm_model','modules')])
+    #config.add_include_dirs([os.path.join(here,'rrtmg_lw_v4.85','gcm_model','src'),
+    #                         os.path.join(here,'rrtmg_lw_v4.85','gcm_model','modules')])
     return config
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -7,17 +7,34 @@ def readme():
     with open('README.rst') as f:
         return f.read()
 
-#  Run the manual f2py build script
-try:
-    from numpy.distutils import fcompiler
-    compiler = fcompiler.get_default_fcompiler()
-    import build_fortran_extensions
-except CompilerNotFound:
-    print 'Cannot find a Fortran compiler, not building Fortran components!'
+# #  Run the manual f2py build script
+# try:
+#     from numpy.distutils import fcompiler
+#     compiler = fcompiler.get_default_fcompiler()
+#     import build_fortran_extensions
+# except CompilerNotFound:
+#     print 'Cannot find a Fortran compiler, not building Fortran components!'
+
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration
+
+    config = Configuration(None, parent_package, top_path)
+    config.set_options(ignore_setup_xxx_py=True,
+                       assume_default_configuration=True,
+                       delegate_options_to_subpackages=True,
+                       quiet=True)
+
+    config.add_subpackage('climlab')
+
+    #config.get_version('numpy/version.py') # sets config.version
+
+    return config
+
 
 if __name__ == '__main__':
     #  Set up climlab with call to setuptools
-    from setuptools import setup
+    #from setuptools import setup
+    from numpy.distutils.core import setup
     from climlab import __version__
     setup(name='climlab',
           version=__version__,
@@ -35,23 +52,23 @@ if __name__ == '__main__':
           author='Brian E. J. Rose',
           author_email='brose@albany.edu',
           license='MIT',
-          packages=[
-            'climlab',
-            'climlab.convection',
-            'climlab.domain',
-            'climlab.dynamics',
-            'climlab.model',
-            'climlab.process',
-            'climlab.radiation',
-            'climlab.radiation.cam3',
-            'climlab.radiation.rrtm',
-            'climlab.radiation.rrtm._rrtmg_sw',
-            'climlab.radiation.rrtm._rrtmg_lw',
-            'climlab.solar',
-            'climlab.surface',
-            'climlab.tests',
-            'climlab.utils',
-          ],
+        #   packages=[
+        #     'climlab',
+        #     'climlab.convection',
+        #     'climlab.domain',
+        #     'climlab.dynamics',
+        #     'climlab.model',
+        #     'climlab.process',
+        #     'climlab.radiation',
+        #     'climlab.radiation.cam3',
+        #     'climlab.radiation.rrtm',
+        #     'climlab.radiation.rrtm._rrtmg_sw',
+        #     'climlab.radiation.rrtm._rrtmg_lw',
+        #     'climlab.solar',
+        #     'climlab.surface',
+        #     'climlab.tests',
+        #     'climlab.utils',
+        #   ],
           install_requires=[
               'numpy',
               'scipy',
@@ -66,10 +83,11 @@ if __name__ == '__main__':
                         '_cam3.so',
                                       ],
             'climlab.radiation.rrtm._rrtmg_sw': ['_rrtmg_sw.so'], # make sure compiled .so files are included
-            'climlab.radiation.rrtm._rrtmg_lw': ['_rrtmg_lw.so'],
+            #'climlab.radiation.rrtm._rrtmg_lw': ['_rrtmg_lw.so'],
             },
           setup_requires=['pytest-runner'],
           tests_require=['pytest'],
           include_package_data=True,
           zip_safe=False,
+          configuration=configuration,
           )
