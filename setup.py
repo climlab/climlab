5372ab001c5b4248dffd67c99bea1eae9c6dc3d8
Brian Rose <brose@albany.edu>
2017-03-23 16:34:14 -0400

    Moving toward a pure numpy.distutils.core.setup experience.

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ if __name__ == '__main__':
             'climlab.solar': ['orbit91'],
             'climlab.radiation.cam3': [
                         os.path.join('data','abs_ems_factors_fastvx.c030508.nc'),
-                        '_cam3.so',
+                        #'_cam3.so',
                                       ],
             #'climlab.radiation.rrtm._rrtmg_sw': ['_rrtmg_sw.so'], # make sure compiled .so files are included
             #'climlab.radiation.rrtm._rrtmg_lw': ['_rrtmg_lw.so'],
