9488d6aae79be21c39e704b8e7ddf3f770a8b5fa
Brian Rose <brose@albany.edu>
2017-03-30 09:51:58 -0400

    Specify list of CAM3 source files directly in setup.py

--- climlab/radiation/cam3/setup.py
+++ climlab/radiation/cam3/setup.py
@@ -15,15 +15,37 @@ def configuration(parent_package='', top_path=None):
         f90flags=['-132', '-r8']
     print f90flags
 
-    here = os.path.abspath(os.path.dirname(__file__))
-    sourcelistfile = os.path.join(here, 'sources_in_order_of_compilation')
-    sourcelist = [line.rstrip() for line in open(sourcelistfile)]
-    sourcelist.append('_cam3.pyf')
+    # here = os.path.abspath(os.path.dirname(__file__))
+    # sourcelistfile = os.path.join(here, 'sources_in_order_of_compilation')
+    # sourcelist = [line.rstrip() for line in open(sourcelistfile)]
+    # sourcelist.append('_cam3.pyf')
+
+    sourcelist = ['_cam3.pyf',
+                'src/pmgrid.F90',
+                'src/prescribed_aerosols.F90',
+                'src/quicksort.F90',
+                'src/abortutils.F90',
+                'src/shr_kind_mod.F90',
+                'src/absems.F90',
+                'src/wv_saturation.F90',
+                'src/aer_optics.F90',
+                'src/cmparray_mod.F90',
+                'src/shr_const_mod.F90',
+                'src/physconst.F90',
+                'src/pkg_cldoptics.F90',
+                'src/gffgch.F90',
+                'src/chem_surfvals.F90',
+                'src/volcrad.F90',
+                'src/radae.F90',
+                'src/radlw.F90',
+                'src/radsw.F90',
+                'src/crm.F90',
+                'Driver.f90']
 
     config = Configuration(package_name='cam3', parent_name=parent_package, top_path=top_path)
     config.add_extension(name='_cam3',
                          sources=sourcelist,
-                         extra_f90_compile_args=f90flags)                         
+                         extra_f90_compile_args=f90flags)
     config.add_include_dirs('src')
     config.add_data_files(os.path.join('data', 'abs_ems_factors_fastvx.c030508.nc'))
     return config
