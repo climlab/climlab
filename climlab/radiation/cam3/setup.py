b140b1d01e3b1cae23421d3dcb8bbd8ee7621089
Brian Rose <brose@albany.edu>
2017-04-04 17:17:07 -0400

    Use source generator functions for Fortran builds.
    This is the recommended way to implement conditional builds with numpy.distutils
    The extensions will not be built if no Fortran compilers are present. This should allow the docs to build again on readthedocs. I hope.

--- climlab/radiation/cam3/setup.py
+++ climlab/radiation/cam3/setup.py
@@ -1,5 +1,8 @@
+from os.path import join
+
+
 def configuration(parent_package='', top_path=None):
-    import os
+    global config
     from numpy.distutils.misc_util import Configuration
     from numpy.distutils.fcompiler import get_default_fcompiler, CompilerNotFound
 
@@ -20,37 +23,50 @@ def configuration(parent_package='', top_path=None):
         print 'No Fortran compiler found, not building the CAM3 radiation module!'
         build = False
 
-    sourcelist = ['_cam3.pyf',
-                'src/pmgrid.F90',
-                'src/prescribed_aerosols.F90',
-                'src/quicksort.F90',
-                'src/abortutils.F90',
-                'src/shr_kind_mod.F90',
-                'src/absems.F90',
-                'src/wv_saturation.F90',
-                'src/aer_optics.F90',
-                'src/cmparray_mod.F90',
-                'src/shr_const_mod.F90',
-                'src/physconst.F90',
-                'src/pkg_cldoptics.F90',
-                'src/gffgch.F90',
-                'src/chem_surfvals.F90',
-                'src/volcrad.F90',
-                'src/radae.F90',
-                'src/radlw.F90',
-                'src/radsw.F90',
-                'src/crm.F90',
-                'Driver.f90']
-
     config = Configuration(package_name='cam3', parent_name=parent_package, top_path=top_path)
-    config.add_data_files(os.path.join('data', 'abs_ems_factors_fastvx.c030508.nc'))
+    config.add_data_files(join('data', 'abs_ems_factors_fastvx.c030508.nc'))
     if build:
         config.add_extension(name='_cam3',
-                         sources=sourcelist,
+                         sources=[cam3_gen_source],
                          extra_f90_compile_args=f90flags,
                          f2py_options=['--quiet'])
         config.add_include_dirs('src')
     return config
+
+def cam3_gen_source(ext, build_dir):
+    '''Add CAM3 fortran source if Fortran 90 compiler available,
+    if no compiler is found do not try to build the extension.'''
+    #  Fortran 90 sources in order of compilation
+    fort90source = ['pmgrid.F90',
+                'prescribed_aerosols.F90',
+                'quicksort.F90',
+                'abortutils.F90',
+                'shr_kind_mod.F90',
+                'absems.F90',
+                'wv_saturation.F90',
+                'aer_optics.F90',
+                'cmparray_mod.F90',
+                'shr_const_mod.F90',
+                'physconst.F90',
+                'pkg_cldoptics.F90',
+                'gffgch.F90',
+                'chem_surfvals.F90',
+                'volcrad.F90',
+                'radae.F90',
+                'radlw.F90',
+                'radsw.F90',
+                'crm.F90',]
+    sourcelist = []
+    sourcelist.append(join(config.local_path,'_cam3.pyf'))
+    for item in fort90source:
+        sourcelist.append(join(config.local_path, 'src', item))
+    sourcelist.append(join(config.local_path,'Driver.f90'))
+    try:
+        config.have_f90c()
+        return sourcelist
+    except:
+        print 'No Fortran 90 compiler found, not building CAM3 extension!'
+        return None
 
 if __name__ == '__main__':
     from numpy.distutils.core import setup
