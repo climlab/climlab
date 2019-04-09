5b72dc0411c2e0053420e464fa86c133fd1dfd7a
Brian Rose <brose@albany.edu>
2019-04-09 12:06:20 -0400

    New load_data_source method implements downland-and-cache strategy.
    Using it on the Berger and Loutre orbital data for now.

--- climlab/solar/orbital/table.py
+++ climlab/solar/orbital/table.py
@@ -3,31 +3,23 @@ import numpy as np
 import os
 import pandas as pd
 import xarray as xr
+from climlab.utils.data_source import load_data_source
 
+#  Two possible sources for the Berger and Loutre 1991 data table
+NCDCpath = "https://www1.ncdc.noaa.gov/pub/data/paleo/climate_forcing/orbital_variations/insolation/orbit91"
+threddspath = "http://thredds.atmos.albany.edu:8080/thredds/fileServer/CLIMLAB/orbital/orbit91"
+#  This gives the full local path to the data file, assuming it's in the same directory
+local_path = os.path.join(os.path.dirname(__file__), "orbit91")
 
-threddspath = 'http://thredds.atmos.albany.edu:8080/thredds/fileServer/CLIMLAB/orbital/orbit91'
-
-def _get_Berger_data():
+def _get_Berger_data(verbose=True):
     '''Read in the Berger and Loutre orbital table as a pandas dataframe, convert to xarray
     '''
-    orbit91_file = 'orbit91'
-    orbit91_url = "https://www1.ncdc.noaa.gov/pub/data/paleo/climate_forcing/orbital_variations/insolation/"
-    try:
-        #  This gives the full path to the data file, assuming it's in the same directory
-        local_path = os.path.dirname(__file__)
-        #path = os.path.join(local_path, orbit91_file)
-        path = threddspath
-        # The first column of the data file is used as the row index, and represents kyr from present
-        orbit91_pd = pd.read_csv(path, delim_whitespace=True, skiprows=1)
-        print('Loaded Berger and Loutre (1991) orbital parameter data from file ' + path)
-    except:
-        print('Failed to load orbital data locally, trying to access it from NCDC archive.')
-        try:
-            path = os.path.join(orbit91_url, orbit91_file)
-            orbit91_pd = pd.read_csv(path, delim_whitespace=True, skiprows=1)
-            print('Accessed Berger and Loutre (1991) orbital data from ' + base_url)
-        except:
-            raise Exception('Failed to load the Berger and Loutre (1991) orbital data.')
+    # The first column of the data file is used as the row index, and represents kyr from present
+    orbit91_pd, path = load_data_source(local_path = local_path,
+                    remote_source_list = [threddspath, NCDCpath],
+                    open_method = pd.read_csv,
+                    open_method_kwargs = {'delim_whitespace': True, 'skiprows':1},
+                    verbose=verbose,)
     #  As xarray structure with the dimension named 'kyear'
     orbit = xr.Dataset(orbit91_pd).rename({'dim_0': 'kyear'})
     #  Now change names
---
+++ climlab/utils/data_source.py
@@ -0,0 +1,38 @@
+from __future__ import division, print_function
+try:
+    from urllib.request import urlretrieve  # Python 3
+except ImportError:
+    from urllib import urlretrieve  # Python 2
+
+
+def load_data_source(local_path,
+               remote_source_list,
+               open_method,
+               open_method_kwargs=None,
+               verbose=True):
+    try:
+        path = local_path
+        data = open_method(path, **open_method_kwargs)
+        if verbose:
+            print('Opened data from {}'.format(path))
+    except FileNotFoundError:
+        #  First try to load from remote sources and cache the file locally
+        for source in remote_source_list:
+            try:
+                urlretrieve(source, local_path)
+                path = local_path
+                data = open_method(path, **open_method_kwargs)
+                if verbose:
+                    print('Data retrieved from {} and saved locally.'.format(source))
+                break
+            except Exception:
+                pass
+        else:
+            # as a final resort, try opening the source remotely
+            for source in remote_source_list:
+                path = source
+                data = open_method(path, **open_method_kwargs)
+                if verbose:
+                    print('Opened data remotely from {}'.format(source))
+    finally:
+        return data, path
