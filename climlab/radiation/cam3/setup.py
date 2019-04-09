d6eecd1979c4cc36f072f14ccb2996e4749203f0
Brian Rose <brose@albany.edu>
2019-04-09 14:54:48 -0400

    Remove all data files from the climlab repository.
    We now rely fully on the download-and-cache functionality provided by climlab.utils.data_source.load_data_source()

--- climlab/radiation/cam3/setup.py
+++ climlab/radiation/cam3/setup.py
@@ -27,7 +27,6 @@ def configuration(parent_package='', top_path=None):
         build = False
 
     config = Configuration(package_name='cam3', parent_name=parent_package, top_path=top_path)
-    config.add_data_files(join('data', 'abs_ems_factors_fastvx.c030508.nc'))
     if build:
         config.add_extension(name='_cam3',
                          sources=[cam3_gen_source],
