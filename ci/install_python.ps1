706501e5a22859e045fa4c343f8e07b0cc9ec3c3
Brian Rose <brose@albany.edu>
2019-07-02 15:46:39 -0400

    Try conda activate on appveyor

--- ci/install_python.ps1
+++ ci/install_python.ps1
@@ -90,7 +90,7 @@ function UpdateConda ($python_home) {
 
 function main () {
     InstallMiniconda $env:PYTHON_VERSION $env:PYTHON_ARCH $env:PYTHON
-#    UpdateConda $env:PYTHON
+    UpdateConda $env:PYTHON
     InstallCondaPackages $env:PYTHON "conda-build jinja2 anaconda-client"
 }
 
