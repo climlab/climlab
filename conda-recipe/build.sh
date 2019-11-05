aba4de75a3d7466316f2b0d84a91066b081895ea
Brian Rose <brose@albany.edu>
2019-11-04 21:56:19 -0500

    Return the other export statements to build.sh ... Seemed to break things on another machine.
    Also change the expected location of the SDK to $HOME/opt/MacOSX10.9.sdk

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -7,10 +7,16 @@ export CPLUS_INCLUDE_PATH="${PREFIX}/include"
 
 # Depending on our platform, shared libraries end with either .so or .dylib
 if [[ `uname` == 'Darwin' ]]; then
+    # Also, included a workaround so that `-stdlib=c++` doesn't go to
+    # `gfortran` and cause problems.
+    #
+    # https://github.com/conda-forge/toolchain-feedstock/pull/8export CFLAGS="${CFLAGS} -stdlib=libc++ -lc++"
+    export CFLAGS="${CFLAGS} -stdlib=libc++ -lc++"
+    export LDFLAGS="-headerpad_max_install_names -undefined dynamic_lookup -bundle -Wl,-search_paths_first -lc++"
     # conda compilers need the MacOSX10.9 SDK and need to be told where they are
     # https://www.anaconda.com/utilizing-the-new-compilers-in-anaconda-distribution-5/
     # Other uses may need to update this if they install MacOSX10.9.sdk somewhere else
-    export CONDA_BUILD_SYSROOT=/opt/MacOSX10.9.sdk
+    export CONDA_BUILD_SYSROOT=$HOME/opt/MacOSX10.9.sdk
 else
     unset LDFLAGS
 fi
