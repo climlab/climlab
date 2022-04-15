8584b1ee5a865afa19952566c6becbfe1cb62830
Brian Rose <brose@albany.edu>
2022-04-15 16:32:11 -0400

    Update import and setup for external cam3 radiation package

--- climlab/radiation/setup.py
+++ climlab/radiation/setup.py
@@ -7,7 +7,6 @@ def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('radiation', parent_package, top_path)
     config.add_subpackage('rrtm')
-    config.add_subpackage('cam3')
     config.add_data_files(os.path.join('data','ozone','README'))
     return config
 
