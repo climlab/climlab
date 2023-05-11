a59808db230e91758abd7546ad5764558dbf209a
HenryDane <aglareb7@gmail.com>
2023-05-11 03:24:15 -0700

    initial version of zonal dynamics processes

---
+++ climlab/dynamics/zonal_advection_diffusion.py
@@ -0,0 +1,47 @@
+from __future__ import division
+import numpy as np
+from .advection_diffusion import AdvectionDiffusion, Diffusion
+from climlab import constants as const
+
+
+class ZonalAdvectionDiffusion(AdvectionDiffusion):
+    """A parent class for meridional advection-diffusion processes.
+    """
+    def __init__(self,
+                 K=0.,
+                 U=0.,
+                 use_banded_solver=False,
+                 prescribed_flux=0.,
+                 **kwargs):
+        super(ZonalAdvectionDiffusion, self).__init__(K=K, U=U,
+                diffusion_axis='lon', use_banded_solver=use_banded_solver, **kwargs)
+        # Conversion of delta from degrees (grid units) to physical length units
+        lambd_stag = np.deg2rad(self.lon_bounds)
+        lambd = np.deg2rad(self.lon)
+        self._Xcenter[...,:] = lambd*const.a
+        self._Xbounds[...,:] = lambd_stag*const.a
+        self._weight_bounds[...,:] = 1.0
+        self._weight_center[...,:] = 1.0
+        #  Now properly compute the weighted advection-diffusion matrix
+        self.prescribed_flux = prescribed_flux
+        self.K = K
+        self.U = U
+
+class ZonalDiffusion(ZonalAdvectionDiffusion):
+    """A parent class for meridional diffusion-only processes,
+    with advection set to zero.
+
+    Otherwise identical to the parent class.
+    """
+    def __init__(self,
+                 K=0.,
+                 use_banded_solver=False,
+                 prescribed_flux=0.,
+                 **kwargs):
+        # Just initialize the AdvectionDiffusion class with U=0
+        super(ZonalDiffusion, self).__init__(
+            U=0.,
+            K=K,
+            prescribed_flux=prescribed_flux,
+            use_banded_solver=use_banded_solver, **kwargs)
+
---
+++ climlab/dynamics/zonal_heat_diffusion.py
@@ -0,0 +1,62 @@
+from __future__ import division
+import numpy as np
+from .zonal_advection_diffusion import ZonalDiffusion
+from climlab import constants as const
+
+class ZonalHeatDiffusion(ZonalDiffusion):
+    '''A 1D diffusion solver for Energy Balance Models.
+
+    Solves the meridional heat diffusion equation
+
+    .. math::
+
+        C \frac{\partial T}{\partial t} = -\frac{1}{\cos\phi} \frac{\partial}{\partial \phi} \left[ -D \cos\phi \frac{\partial T}{\partial \phi} \right]
+
+    on an evenly-spaced latitude grid, with a state variable :math:`T`,
+    a heat capacity :math:`C` and diffusivity :math:`D`.
+
+    Assuming :math:`T` is a temperature in K or degC, then the units are:
+
+    - :math:`D` in W m-2 K-1
+    - :math:`C` in J m-2 K-1
+
+    :math:`D` is provided as input, and can be either scalar
+    or vector defined at latitude boundaries.
+
+    :math:`C` is normally handled automatically for temperature state variables in CLIMLAB.
+    '''
+    def __init__(self,
+                 D=0.555,  # in W / m^2 / degC
+                 use_banded_solver=False,
+                 **kwargs):
+        #  First just use a dummy value for K
+        super(ZonalHeatDiffusion, self).__init__(K=1.,
+                        use_banded_solver=use_banded_solver, **kwargs)
+        #  Now initialize properly
+        self.D = D
+        self.add_diagnostic('heat_transport', 0.*self.diffusive_flux)
+        self.add_diagnostic('heat_transport_convergence', 0.*self.flux_convergence)
+
+    @property
+    def D(self):
+        return self._D
+    @D.setter
+    def D(self, Dvalue):
+        self._D = Dvalue
+        self._update_diffusivity()
+
+    def _update_diffusivity(self):
+        for varname, value in self.state.items():
+            heat_capacity = value.domain.heat_capacity
+        # diffusivity in units of m**2/s
+        self.K = self.D / heat_capacity * const.a**2
+
+    def _update_diagnostics(self, newstate):
+        super(ZonalHeatDiffusion, self)._update_diagnostics(newstate)
+        for varname, value in self.state.items():
+            heat_capacity = value.domain.heat_capacity
+        coslat_bounds = np.moveaxis(self._weight_bounds,-1,self.diffusion_axis_index)
+        self.heat_transport[:] = (self.diffusive_flux * heat_capacity *
+            2 * np.pi * const.a * coslat_bounds * 1E-15) # in PW
+        self.heat_transport_convergence[:] = (self.flux_convergence *
+                        heat_capacity)  # in W/m**2
