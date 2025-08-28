e64b385aa743d1b7d74a17b9330769f0f6fe516e
Brian Rose <brose@albany.edu>
2025-08-28 11:01:41 -0400

    Fix up basic test

--- climlab/tests/test_2D_advdiff.py
+++ climlab/tests/test_2D_advdiff.py
@@ -1,11 +1,12 @@
-import numpy as np
-import pytest
+from climlab.domain import column_state
 from climlab.dynamics import TwoDimensionalAdvectionDiffusion
+import pytest
+
 
 @pytest.mark.fast
 def test_2D_advection_diffusion_basic():
     '''Can we initialize a 2D lat-pressure model and step forward with zero diffusion and advection?'''
-    state = climlab.column_state(num_lev=30, num_lat=40, water_depth=10.)
+    state = column_state(num_lev=30, num_lat=40, water_depth=10.)
     m = TwoDimensionalAdvectionDiffusion(name='Advection-Diffusion', 
                                          state={'Tatm': state['Tatm']},
                                          Kyy=0.,
